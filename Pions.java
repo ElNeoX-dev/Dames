9aa7c9f878cad25091ce5ec64ad6cf167c7813c2
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2021-05-10 11:44:57 +0200

    Update Pions.java

---
+++ Pions.java
@@ -0,0 +1,19 @@
+public class Pions {
+    int x;
+    int y;
+    boolean couleur;
+    boolean estDame;
+    boolean estVivant;
+
+
+    public Pions(int x, int y, boolean couleur) {
+        this.x = x;
+        this.y = y;
+        this.couleur = couleur;
+        estDame = false;
+        estVivant = true;
+    }
+    
+} 
+
+
