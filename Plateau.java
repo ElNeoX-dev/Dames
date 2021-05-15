38e88dfb8c61e5ae56b29f7d4179efa0e8965a5c
Shaarllun <78822946+Shaarllun@users.noreply.github.com>
2021-05-15 15:58:38 +0200

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -1,6 +1,6 @@
 public class Plateau {
-    int taille;
-    Pions[][] cases;
+    private int taille;
+    private Pions[][] cases;
 
     public Plateau() {
         this.taille = 10;
@@ -11,9 +11,15 @@ public class Plateau {
     public void afficherPlateau() {
 
     }
+
+    public Pions[][] getCases (){
+        return this.cases;
+    }
     
-    public boolean PeutManger(Pions pion) {
-        
+    public boolean peutManger(Pions pion) {
+        int x = pion.getX;
+        int y = pion.getY;
+        return (cases [x+1][y+1].getVivant || cases [x+1][y-1].getVivant || cases [x-1][y+1].getVivant || cases [x-1][y-1].getVivant);
     }
 
     public void majPlateau(Pions pion) {
