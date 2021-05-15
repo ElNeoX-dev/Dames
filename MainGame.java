8d22b10a20bd0291041ecf4a323b51d1e7b3e17a
ElNeoX-dev <61790793+ElNeoX-dev@users.noreply.github.com>
2021-05-15 15:23:11 +0200

    Update MainGame.java

--- MainGame.java
+++ MainGame.java
@@ -8,22 +8,26 @@ public class MainGame {
 
     public static void initPlateau(Plateau plateau, int nbPions) {
         int compteur = 0;
+        Pions[][] stockPions = new Pions[2][nbPions];
         for (int j = 0; j < (plateau.taille / 2 - 1); j++) {
             for(int i = j % 2; i < plateau.taille; i += 2 ) {
-                if (compteur < nbPions / 2){
-                    plateau.majPlateau(new Pions (i, j, true));
+                if (compteur < nbPions){
+                    stockPions[0][compteur] = new Pions (i, j, true, true)
+                    plateau.majPlateau(stockPions[0][compteur]);
                     compteur++;
                 }
             }
         }
+        compteur = 0;
         for (int j = plateau.taille - 1; j >= plateau.taille / 2 + 1 ; j--) {
             for(int i = 1 - (j % 2); i < plateau.taille; i += 2) {
                 if (compteur < nbPions){
-                    plateau.majPlateau(new Pions (i, j, false));
+                    stockPions[1][compteur] = new Pions (i, j, true, true)
+                    plateau.majPlateau(stockPions[1][compteur]);
                     compteur++;
                 }
             }
         }
-        
+        plateau.remplissagePlateau();
     }
 }
