53b8e5f84e36331235a151d5fe4835019dae756f
Shaarllun <78822946+Shaarllun@users.noreply.github.com>
2021-05-30 17:08:26 +0200

    Revert "Merge branch 'main' of https://github.com/ElNeoX-dev/Dames into main"
    This reverts commit f899f19efbddf2d91bee4e156bf00bbfe83603e8, reversing
    changes made to fdfdec4d9a86556e0bff831e57057a94c55ef870.

--- Pions.java
+++ Pions.java
@@ -6,7 +6,7 @@ public class Pions {
     boolean estVivant;
 
 
-    public Pions(int x, int y, boolean estBlanc, boolean estVivant) {
+    public Pions(int y, int x, boolean estBlanc, boolean estVivant) {
         this.x = x;
         this.y = y;
         this.estBlanc = estBlanc;
@@ -49,12 +49,6 @@ public class Pions {
         estDame = true;
     }
 
-    public boolean mange(Pions cible, Plateau plateau) {
-
-    }
-
-
-
     public int[] posManger(Pions cible){
         int j;
         int i;
@@ -68,7 +62,7 @@ public class Pions {
         } else {
             i = 1;
         }
-        if(cible.y + j < 0 || cible.y + j > 9 || cible.x + i < 0 || cible.x + i >0){
+        if(cible.y + j < 0 || cible.y + j > 9 || cible.x + i < 0 || cible.x + i > 9){
             int[] pos = {0, 1};
             return pos;
         } else {
@@ -78,7 +72,7 @@ public class Pions {
 
     }
 
-    public boolean cheminLibre(Pions cible) {
+    public boolean cheminLibre(Pions cible, Plateau plateau) {
         int k;
         int l;
         if(this.y > cible.y){
@@ -93,31 +87,42 @@ public class Pions {
         }
         int[][] condition = {{1, 1},{1, -1},{-1, 1},{-1, -1}};
         int[] cas = {k, l};
-        int numcas;
+        int numcas = 5;
         
         for(int i = 0; i < condition.length; i++) {
             if (cas[0] == condition[i][0] && cas[1] == condition[i][1]) {
                 numcas = i;
             }
         }
+        int[] arrivee = new int [2];
+        int[] depart = new int [2];
         if (numcas == 0) {
-            int[] depart = {this.x, this.y};
-            int[] arrivee = {cible.x, cible.y};
+            depart[0] = this.x;
+            depart[1] = this.y;
+            arrivee[0] = cible.x;
+            arrivee[1] = cible.y;
         } else if(numcas == 1) {
-            int[] depart = {cible.x, this.y};
-            int[] arrivee = {this.x, cible.y};
+            depart[0] = cible.x;
+            depart[1] = this.y;
+            arrivee[0] = this.x;
+            arrivee[1] = cible.y;
         } else if(numcas == 2) {
-            int[] depart = {this.x, cible.y};
-            int[] arrivee = {cible.x, this.y};
+            depart[0] = this.x;
+            depart[1] = cible.y;
+            arrivee[0] = cible.x;
+            arrivee[1] = this.y;
+
         } else {
-            int[] depart = {cible.x, cible.y};
-            int[] arrivee = {this.x, this.y};
+            depart[0] = cible.x;
+            depart[1] = cible.y;
+            arrivee[0] = this.x;
+            arrivee[1] = this.y;
         }
         int distx;
         int disty;
 
         for(int j = depart[1]; j < arrivee[1]; j++) {
-            for(int i = depart[0]; i < arivee[0]; i++) {
+            for(int i = depart[0]; i < arrivee[0]; i++) {
                 disty = (int) (Math.abs(j-this.y));
                 distx = (int) (Math.abs(i-this.x));
                 if(disty == distx && plateau.getCases(i, j).estVivant) {
@@ -126,6 +131,10 @@ public class Pions {
             }
         }
         return true;
+    }
+
+    public int dist(Pions cible) {
+        return (int) ( (Math.abs(this.x-cible.x) + Math.abs(this.y-cible.y)  ) );
     }
 } 
 
--- Plateau.java
+++ Plateau.java
@@ -46,8 +46,34 @@ public class Plateau {
         if(!pion.getestDame()){
             int x = pion.getX();
             int y = pion.getY();
-            for(int j = y - 1; j <= y+1; j += 2){
-               for(int i = x-1; i <= x+1; i += 2){              
+            int iMin;
+            int iMax;
+            int jMin;
+            int jMax;
+
+            if(x < 2) {
+                iMin = x;
+            } else {
+                iMin = x - 1;
+            }
+            if(y < 2) {
+                jMin = y;
+            } else {
+                jMin = y - 1;
+            }
+            if(x > 7) {
+                iMax = x;
+            } else {
+                iMax = x + 1;
+            }
+            if(y > 7) {
+                jMax = y;
+            } else {
+                jMax = y + 1;
+            }
+
+            for(int j = jMin; j <= jMax; j++){
+               for(int i = iMin; i <= iMax; i++){              
                    if(cases[j][i].getestVivant() && (pion.getestBlanc() != cases[j][i].getestBlanc())){
                        int[] posCible = pion.posManger(cases[j][i]);
                        if(!cases[posCible[0]][posCible[1]].estVivant) {
@@ -66,7 +92,7 @@ public class Plateau {
                     cible = cases[j][i];
                     disty = (int) (Math.abs(j-pion.getY()));
                     distx = (int) (Math.abs(i-pion.getX()));
-                    if(distx == disty && cible.getestVivant() && (pion.getestBlanc() != cible.getestBlanc()) && pion.cheminLibre(cible)) {
+                    if(distx == disty && cible.getestVivant() && (pion.getestBlanc() != cible.getestBlanc()) && pion.cheminLibre(cible, this)) {
                         int[] posCible = pion.posManger(cible);
                         if(!cases[posCible[0]][posCible[1]].estVivant && posCible[0] != 0 && posCible[1]!= 1) {
                             return true;
@@ -100,11 +126,30 @@ public class Plateau {
         }
     }
 
-    public boolean bouger(Pions pion) {
-        if(peutManger(pion)) {
-
+    public boolean bouger(Pions pion, Pions cible) {
+        int distLim;
+        if(!pion.getestDame()) {
+            distLim = 2;
         } else {
-            
+            distLim = 100;
+        }
+        int a = pion.getY();
+        int b = pion.getX();
+        int [] posManger = pion.posManger(cible);
+        if(peutManger(pion) && (cible).getestVivant() && posManger[0] != 0 && posManger[1] != 1 && !cases[posManger[0]][posManger[1]].estVivant && pion.dist(cible) <= distLim && pion.dist(cible) >= 2) {
+            pion.setPos(posManger[0], posManger[1]);
+            update(a, b, pion);
+            cible.setestVivant();
+            afficher();
+            return true;
+        } else if(!cible.estVivant && pion.dist(cible) <= distLim && !peutManger(pion)) {
+            pion.setPos(cible.getX(), cible.getY());
+            update(a, b, pion);
+            afficher();
+            return true;
+        } else {
+            afficher();
+            return false;
         }
     }
 
@@ -136,6 +181,12 @@ public class Plateau {
             }
         }
         return true;
+    }
+
+    public void update(int a, int b, Pions pion) {
+        cases[pion.getY()][pion.getX()] = pion;
+        cases[a][b] = new Pions(a, b, true, false);
+
     }
 
 }
